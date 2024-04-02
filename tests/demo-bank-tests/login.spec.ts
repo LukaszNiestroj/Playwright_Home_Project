ff48e36a38621f235baec3ef06ea02b73c2aab4d
Łukasz Niestrój <lukasz.niestroj@tensquaregames.com>
2024-04-02 20:17:06 +0200

    added test with ussuccessful login with short password

--- tests/demo-bank-tests/login.spec.ts
+++ tests/demo-bank-tests/login.spec.ts
@@ -1,7 +1,7 @@
 import { test, expect } from '@playwright/test';
 
 test.describe('User login to Demobank', () => {
-  test('login with correct credentials', async ({ page }) => {
+  test('Login with correct credentials', async ({ page }) => {
     await page.goto('https://demo-bank.vercel.app/');
     await page.getByTestId('login-input').click();
     await page.getByTestId('login-input').fill('tester12');
@@ -14,7 +14,7 @@ test.describe('User login to Demobank', () => {
   });
 
 
-  test('login with incorrect credentials with incorrect username', async ({ page }) => {
+  test('Unsuccessful login with to short username', async ({ page }) => {
     await page.goto('https://demo-bank.vercel.app/');
     await page.getByTestId('login-input').click();
     await page.getByTestId('login-input').fill('tester');
@@ -23,4 +23,16 @@ test.describe('User login to Demobank', () => {
 
     await expect(page.getByTestId('error-login-id')).toHaveText('identyfikator ma min. 8 znaków');
   });
-});
+
+  test('Unsuccessful login with to short password', async ({ page }) => {
+    await page.goto('https://demo-bank.vercel.app/');
+    await page.getByTestId('login-input').click();
+    await page.getByTestId('login-input').fill('tester12');
+    await page.getByTestId('password-input').click();
+    await page.getByTestId('password-input').fill('test');
+    await page.getByTestId('password-input').blur();
+  
+
+    await expect(page.getByTestId('error-login-password')).toHaveText('hasło ma min. 8 znaków');
+  });
+});  
