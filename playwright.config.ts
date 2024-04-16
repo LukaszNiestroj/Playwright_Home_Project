ddf575b237580297a29dc9a5d6cd7b9ac545b0c3
Łukasz Niestrój <lukasz.niestroj@tensquaregames.com>
2024-04-16 19:46:14 +0200

    added optional trace viewer allways on

--- playwright.config.ts
+++ playwright.config.ts
@@ -28,6 +28,7 @@ export default defineConfig({
 
     /* Collect trace when retrying the failed test. See https://playwright.dev/docs/trace-viewer */
     trace: 'retain-on-failure',
+    // trace: 'on',
     // headless: false,
     video: 'retain-on-failure',
   },
